71cc7a4fa8ee2036061da7060a323c984c18ce7c
Michael Comella <michael.l.comella@gmail.com>
2020-12-17 13:30:42 -0800

    Various changes to perfherder-analyze.

--- perfherder-analyze.py
+++ perfherder-analyze.py
@@ -4,9 +4,10 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 """TODO: explain me.
+Typical run:
+    cat [file-with-array-from-fnprms|perfherder_data line] | ../perf-tools/perfherder-analyze.py [--fnprms] --save-dir artifacts --save-id remove-intents
 """
 
-import fileinput
 import json
 import os
 import statistics
@@ -21,6 +22,7 @@ def parse_args():
     parser.add_argument('--save-id', help='an identifier to save the files as')
     parser.add_argument('--no-graph', action='store_true', help='disables the graph')
     parser.add_argument('--force', action='store_true', help='completes analysis if save-id already exists')
+    parser.add_argument('--fnprms', action='store_true', help="Read output from FNPRMS instead of perfherder")
     return parser.parse_args()
 
 
@@ -53,10 +55,17 @@ def get_perfherder_replicates(raw_data):
     return suite['subtests'][0]['replicates']
 
 
-def analyze_replicates(replicates):
+def get_fnprms_replicates(input):
+    import ast
+    values = ast.literal_eval(''.join(input))
+    return [e * 1000 for e in values] # convert to ms
+
+def analyze_replicates(replicates, is_fnprms):
+    test = 'fnprms-MAIN' if is_fnprms else 'perftest-VIEW'
     return {
-        'test': 'perftest-VIEW',
+        'test': test,
         'replicates': replicates,
+        'count': len(replicates),
         'mean': statistics.mean(replicates),
         'median': statistics.median(replicates)
     }
@@ -66,7 +75,8 @@ def graph(analysis, save_path, no_graph):
     # Assumes save path is writeable (i.e. dir exists).
     replicates = analysis['replicates']
     #pyplot.plot(range(len(replicates)), replicates)
-    y = [5 for _ in replicates]
+    #y = [5 for _ in replicates]
+    y = range(len(replicates))
     pyplot.scatter(replicates, y)
     if save_path:
         pyplot.savefig(save_path + '-graph.pdf')
@@ -80,22 +90,40 @@ def save_analysis(analysis, save_path):
     with open(path, 'w') as f:
         pprint(analysis, stream=f, compact=True)
 
+    # we can just read in analysis results with ast.literal_eval.
+    #json_path = save_path + '-analysis.json'
+    #with open(json_path, 'w') as f:
+    #    json.dump(analysis, f)
 
-def save_input(raw_data, save_path):
+
+def save_input(input, save_path):
     # Assumes save path is writeable (i.e. dir exists).
     path = save_path + '-input.txt'
     with open(path, 'w') as f:
-        print(raw_data, file=f)
+        print(input, file=f)
+
+
+def save_json(raw_data, save_path):
+    # Assumes save path is writeable (i.e. dir exists).
+    path = save_path + '-input.json'
+    with open(path, 'w') as f:
+        json.dump(raw_data, f)
 
 
 def main():
     args = parse_args()
     validate_args(args)
 
-    raw_data = get_perfherder_data(sys.stdin)
-    replicates = get_perfherder_replicates(raw_data)
-    analysis = analyze_replicates(replicates)
+    input = [l for l in sys.stdin]
+    if not args.fnprms:
+        raw_data = get_perfherder_data(input)
+        replicates = get_perfherder_replicates(raw_data)
+    else:
+        replicates = get_fnprms_replicates(input)
+    analysis = analyze_replicates(replicates, args.fnprms)
 
+
+    # tood: helper fun
     save_path = None
     if args.save_dir and args.save_id:
         os.makedirs(args.save_dir, exist_ok=True)
@@ -107,7 +135,9 @@ def main():
             raise Exception('File at {} already exists. Unable to save. Exiting...'.format(args.save_id))
 
         save_analysis(analysis, save_path)
-        save_input(raw_data, save_path)
+        save_input('\n'.join(input), save_path) # TODO: janky
+        # Input JSON seems unnecessary if we save analysis as json.
+        #save_json(raw_data, save_path)
     pprint(analysis, compact=True)
     graph(analysis, save_path, args.no_graph)
 
