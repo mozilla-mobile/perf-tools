91793c1313a34f8ed9e5aeb18623b7301d149434
Michael Comella <michael.l.comella@gmail.com>
2021-03-10 11:11:24 -0800

    No issue: merge average-logcat into average_times as --from-logcat.

--- average_times.py
+++ average_times.py
@@ -20,10 +20,19 @@ For example, its contents might be:
 844
 """
 
+LOGCAT_MATCH_STR = 'average '
+LOGCAT_EXPECTED_FORMAT = '2020-05-04 15:15:50.340 10845-10845/? E/lol: average 37'
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=DESC, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description=DESC, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("path", help="path to a file with duration measurements separated by newlines")
+
+    parser.add_argument("--from-logcat", action="store_true", help="""reads a file with lines from logcat instead of durations separated by newlines.
+The logcat message must be \"average <value>\": other log values such as tags are ignored. For example:
+
+{}""".format(LOGCAT_EXPECTED_FORMAT))
+
     parser.add_argument(
         "--print-github-table-header", action="store_true",
         help="prints the input-agnostic header for --print-github-table-row args and exits. " +
@@ -39,6 +48,21 @@ def read_from_file_separated_by_newlines(path):
     with open(path) as f:
         contents = f.read()
     return [float(r) for r in contents.split('\n') if r]  # trailing if is used to remove empty lines.
+
+
+def read_from_logcat_file(path):
+    measurements = []
+    with open(path) as f:
+        for line in f:
+            # The message will be after the last colon because we demand a certain formatting.
+            message_text = line[line.rfind(': ') + 2:]  # +2 to move past ': '.
+
+            if message_text and message_text.startswith(LOGCAT_MATCH_STR):
+                measurements.append(float(message_text[len(LOGCAT_MATCH_STR):]))
+
+    if len(measurements) == 0:
+        print('WARN: no lines matched. expected format like:\n    {}'.format(LOGCAT_EXPECTED_FORMAT))
+    return measurements
 
 
 def to_stats(measurements_arr):
@@ -67,7 +91,10 @@ def main():
         print_github_table_header()
         exit(0)
 
-    measurement_arr = read_from_file_separated_by_newlines(args.path)
+    if args.from_logcat:
+        measurement_arr = read_from_logcat_file(args.path)
+    else:  # default operation: newline file format
+        measurement_arr = read_from_file_separated_by_newlines(args.path)
     stats = to_stats(measurement_arr)
 
     if args.print_github_table_row:
