3d0c002330837e5b0dc638645088f3f4fe89e371
Marc Leclair <marc.leclair0113@gmail.com>
2021-07-26 20:31:35 -0400

    Nit patch

--- backfill.py
+++ backfill.py
@@ -1,8 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import argparse
 import urllib.request
 import subprocess
 import analyze_durations
 import os
+import sys
 from pathlib import Path
 from datetime import datetime, timedelta
 
@@ -22,7 +27,7 @@ KEY_DATETIME = "date"
 DATETIME_FORMAT = "%Y.%m.%d"
 
 
-def prase_args():
+def parse_args():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
 
     parser.add_argument("path_to_startup_script",
@@ -30,14 +35,17 @@ def prase_args():
     parser.add_argument("package_id", help="Package id associated with the apk to install / uninstall")
     parser.add_argument("start", type=lambda date: datetime.strptime(date, DATETIME_FORMAT),
                         help="Date to start the backfill")
-    parser.add_argument("-ed", "--enddate", type=lambda date: datetime.strptime(date, DATETIME_FORMAT),
+    parser.add_argument("-e", "--enddate", type=lambda date: datetime.strptime(date, DATETIME_FORMAT),
+                        default=datetime.now(),
                         help="end date to backfill until.If empty, default will be the current date")
-    parser.add_argument("-rp", "--repository_path",
-                        help="Path to the repository where the commits will be gotten from")
     parser.add_argument("-t", "--type", required=True, choices=["nightly", "commits"],
                         help="The type of system the backfill should run performance analysis on")
-    parser.add_argument("-cl", "--cleanup", choices=[True, False], default=False,
+
+    parser.add_argument("-r", "--repository_path",
+                        help="Path to the repository where the commits will be gotten from")
+    parser.add_argument("-c", "--cleanup", action="store_true",
                         help="Remove all apks downloaded since they can takeup lots of space")
+
     return parser.parse_args()
 
 
@@ -45,66 +53,57 @@ def fetch_nightly(download_date):
     download_date_string = datetime.strftime(download_date, DATETIME_FORMAT)
     nightly_url = NIGHTLY_BASE_URL.format(date=download_date_string)
     filename = "nightly_" + download_date_string.replace(".", "_") + ".apk"
-    # TODO: Could add build type, architecture, etc...
-    apk_metadata = {KEY_NAME: filename, KEY_DATETIME: download_date}
     try:
         urllib.request.urlretrieve(nightly_url, filename=filename)
     except urllib.error.HTTPError as err:
-        apk_metadata = None
         if err.code == 404:
-            print("\n\nThe apk for {date} is not available at this {url}".format(date=download_date, url=nightly_url))
-    return apk_metadata
+            print("\n\nThe apk for {date} is not available at this {url}".format(date=download_date, url=nightly_url),
+                  file=sys.stderr)
+        return None
+
+    # TODO: Could add build type, architecture, etc...
+    return {KEY_NAME: filename, KEY_DATETIME: download_date}
 
 
 def get_date_array_for_range(startdate, enddate):
-    date_array = []
-    delta = (enddate - startdate).days + 1
-    for i in range(delta):
-        date_array.append(startdate + timedelta(days=i))
-    return date_array
+    delta_dates = (enddate - startdate).days + 1
+    return [startdate + timedelta(days=i) for i in range(delta_dates)]
 
 
 def download_nightly_for_range(array_of_dates):
-    apk_metadata_array = []
-    for date in array_of_dates:
-        # TODO if file exist and no -f option
-        apk_metadata = fetch_nightly(date)
-        if apk_metadata is not None:
-            apk_metadata_array.append(apk_metadata)
-    return apk_metadata_array
+    # TODO if file exist and no -f option
+    apk_metadata_array = [fetch_nightly(date) for date in array_of_dates]
+    return [e for e in apk_metadata_array if e is not None]
 
 
 def install_apk(apk_build_path):
-    install_output = subprocess.run(["adb", "install", apk_build_path], check=False, capture_output=True)
-    if len(install_output.stderr.decode('utf-8').strip()) == 0:
-        return True
-    else:
-        print("""\n\n Something went wrong while installing the following apk:{apk} . The associated error message was:
-        \n {error}""".format(apk=apk_build_path, error=install_output.stderr))
+    install_proc = subprocess.run(["adb", "install", apk_build_path], check=False, capture_output=True)
+    if install_proc.returncode != 0:
+        print(("\n\nSomething went wrong while installing the following apk: {apk} . The associated error message was:"
+               "\n\n {error}".format(apk=apk_build_path, error=install_proc.stderr.strip(b'\n'))),
+              file=sys.stderr)
         return False
+    return True
 
 
 def uninstall_apk(package_id):
-    uninstall_output = subprocess.run(["adb", "uninstall", package_id], check=False, capture_output=True)
-    if len(uninstall_output.stderr.decode('utf-8').strip()) != 0:
-        print("""\n\n Something with the uninstalling {package_id} went wrong. The associated error message was:
-        \n {error}""".format(package_id=package_id, error=uninstall_output.stderr.decode('utf-8').strip("\n")))
+    uninstall_proc = subprocess.run(["adb", "uninstall", package_id], check=False, capture_output=True)
+    if uninstall_proc.returncode != 0:
+        print(("\n\nSomething with the uninstalling {package_id} went wrong. The associated error message was:\n\n"
+               " {error}".format(package_id=package_id, error=uninstall_proc.stderr.strip(b"\n"))),
+              file=sys.stderr)
 
 
 def run_measure_start_up_script(path_to_measure_start_up_script, durations_output_path):
-    output = subprocess.run([path_to_measure_start_up_script, "nightly", "cold_view_nav_start", durations_output_path],
-                            capture_output=True, check=False)
-
-    if output.stderr is not None:
-        print(output.stderr.decode('utf-8').strip("\n"))
+    subprocess.run([path_to_measure_start_up_script, "nightly", "cold_view_nav_start", durations_output_path],
+                   stdout=subprocess.PIPE, check=False)
 
 
 def analyze_nightly_for_one_build(package_id, path_to_measure_start_up_script, apk_metadata):
     uninstall_apk(package_id)
 
-    was_install_successfull = install_apk(apk_metadata[KEY_NAME])
-
-    if was_install_successfull:
+    was_install_successful = install_apk(apk_metadata[KEY_NAME])
+    if was_install_successful:
         Path(BACKFILL_DIR).mkdir(parents=True, exist_ok=True)
 
         apk_name = apk_metadata[KEY_NAME].split(".")[0]
@@ -115,19 +114,20 @@ def analyze_nightly_for_one_build(package_id, path_to_measure_start_up_script, a
 
         run_measure_start_up_script(path_to_measure_start_up_script, durations_output_path)
         get_result_from_durations(durations_output_path, analyzed_durations_path)
-    else:
-        print("Installation for the apk {apk} was unsuccessfull".format(apk=apk_metadata[KEY_NAME]))
 
 
 def get_result_from_durations(start_up_durations_path, analyzed_path):
     try:
         filetype = analyze_durations.detect_filetype(start_up_durations_path)
-        measurement_arr = filetype.read_from(start_up_durations_path)
-        stats = analyze_durations.to_stats(measurement_arr)
-        analyze_durations.save_output(stats, analyzed_path)
-    except Exception:
+    except FileNotFoundError:
         print(("The file {file} doesn't exist, this is probably due to a failure in running"
-               + "the measure_start_up.py for the apk with the according date").format(file=start_up_durations_path))
+               "the measure_start_up.py for the apk with the according date").format(file=start_up_durations_path),
+              file=sys.stderr)
+        return
+
+    measurement_arr = filetype.read_from(start_up_durations_path)
+    stats = analyze_durations.to_stats(measurement_arr)
+    analyze_durations.save_output(stats, analyzed_path)
 
 
 def run_performance_analysis_on_nightly(package_id, path_to_measure_start_up_script, array_of_apk_path):
@@ -141,26 +141,18 @@ def cleanup(array_of_apk_path):
 
 
 def main():
-    args = prase_args()
-
-    path_to_measure_start_up_script = args.path_to_startup_script
-    package_id = args.package_id
-    startdate = args.start
-    enddate = args.enddate
-    if args.enddate is None:
-        enddate = datetime.now()
-
+    args = parse_args()
     if args.type == "commits" and args.fenix_path is None:
         raise Exception("Provide the path to your fenix repository to run this script with the commits option")
 
-    array_of_dates = get_date_array_for_range(startdate, enddate)
+    array_of_dates = get_date_array_for_range(args.start, args.enddate)
 
     if args.type == "nightly":
         array_of_apk_metadata = download_nightly_for_range(array_of_dates)
 
-    run_performance_analysis_on_nightly(package_id, path_to_measure_start_up_script, array_of_apk_metadata)
+    run_performance_analysis_on_nightly(args.package_id, args.path_to_startup_script, array_of_apk_metadata)
 
-    if cleanup is True:
+    if args.cleanup is True:
         cleanup(array_of_apk_metadata)
 
 
